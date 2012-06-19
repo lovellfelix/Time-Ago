f835b8aaa98646793bc6f847791ee2278543ebdd
Lovell Felix <lovell.felix@gmail.com>
2012-06-19 11:37:14 -0500

    add comments index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 $time = strtotime('2012-06-13 15:25:43');  // Time to convert 
-
-echo '- '.timeStamp($time).' ago';
+· 8 minutes ago 
+echo ' · '.timeStamp($time).' ago '; // Prints timestamp
 
 function timeStamp ($time)
 {
 
-    $time = time() - $time; // To get the time since that moment
+    $time = time() - $time; // Calculate the time since that moment ago
 
-    $tokens = array (
+    $tokens = array (  // Default time units.
         31536000 => 'year',
         2592000 => 'month',
         604800 => 'week',
@@ -21,7 +21,7 @@ function timeStamp ($time)
     foreach ($tokens as $unit => $text) {
         if ($time < $unit) continue;
         $numberOfUnits = floor($time / $unit);
-        return $numberOfUnits.' '.$text.(($numberOfUnits>1)?'s':'');
+        return $numberOfUnits.' '.$text.(($numberOfUnits>1)?'s':''); // Automatically de-pluralized if, say there is only 1 year.
     }
 
 
